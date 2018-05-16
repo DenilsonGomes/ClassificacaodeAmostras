5f47a9da7d836109ceff2290098c57b3a282f70f
Denilson Gomes <denilsongomesdg97@gmail.com>
2018-05-16 02:01:13 -0300

    Arquivos codigo fonte

---
+++ ClassLeave.m
@@ -0,0 +1,20 @@
+%Autor: Denilson Gomes Vaz da Silva
+%Graduando em Engenharia da Computação
+%Inteligência Computacional - Dr. Jarbas Joaci
+%Classificar a base database_pap
+
+clear %limpa as variaveis
+clc %limpa o visor
+
+load database_pap.dat %carrega a base de arquivos
+
+amostras = database_pap'; %X recebe os vetores da base
+[tam,~] = size(database_pap); %calcula o tamanho da base de dados
+classes = zeros(2,tam); %a classe é um vetor coluna, classe 0 = [1 0]T e classe 1 = [0 1]T
+classes(1,1:242) = 1; %amostras da classe 0
+classes(2,243:tam) = 1; %amostras da classe 1
+
+%leave-one-out
+acertos = leaveoneout(amostras, classes);
+taxa = (acertos/917)*100;
+fprintf('Porcentagem de acertos sem a normalização: %.4f\n',taxa);
---
+++ leaveoneout.m
@@ -0,0 +1,29 @@
+%Autor: Denilson Gomes Vaz da Silva
+%Graduando em Engenharia da Computação
+%Inteligência Computacional - Dr. Jarbas Joaci
+%Função que executa o leave-one-out e retorna o numero de acertos
+
+%função recebe (amostras, classes) e retorna o numero de acertos
+function acertos = leaveoneout(amostras, classes)
+[~,tam] = size(amostras);
+acertos = 0;
+for i=1:tam
+    X = amostras; %x recebe todas as amostras
+    X(:,i) = []; %removemos a amostra i para teste
+    Y = classes; %class recebe o rotulo das amostras
+    Y(:,i) = []; %retiramos o rotulo da amostra teste
+    
+    A = (Y*X')/(X*X'); %calculo da matriz transformação
+    y = A*(amostras(:,i)); %calculo do vetor saida y
+    
+    if y(1) > y(2) %se a primeira coordenada for maior
+        if classes(1,i) == 1 %e o elemento for da classe 0
+            acertos = acertos + 1; %acrescente acertos
+        end    
+    else %se a segunda coordenada for maior
+        if classes(2,i) == 1 %e o elemento for da classe 1
+            acertos = acertos + 1; %acrescente acertos
+        end    
+    end  
+end
+acertos;
